0baa6e7f835149df821d56b8a3ac77e4e1f3d506
Aleksandra Moldoch <a.moldoch94@gmail.com>
2019-12-02 22:48:29 +0100

    Change control feedback message.

--- src/containers/Auth/LoginSchema.js
+++ src/containers/Auth/LoginSchema.js
@@ -10,8 +10,8 @@ const loginSchema = Yup.object().shape({
         .required('Login id required'),
     password: Yup.string()
         .min(6, 'Password has to be longer than 6 characters!')
-        .matches(re, 'Password has to be longer than 6 characters!')
-        .required('Password must contain uppercase character, lowercase character and a number!')
+        .matches(re, 'Password must contain uppercase character, lowercase character and a number!')
+        .required('Password has to be longer than 6 characters!')
 });
 
 export default loginSchema;
